28f9a7cf7fd15a9d2c1b0cfbd93a764ef0138b9f
gobetti-caio <124630531+gobetti-caio@users.noreply.github.com>
2023-12-04 23:43:09 -0300

    Update dijkstra.h

--- Trabalho 2/dijkstra.h
+++ Trabalho 2/dijkstra.h
@@ -7,8 +7,6 @@
 
 
 Vector *solve_dijkstra(Problem *problem);
-void update_path(Vector *paths, PossiblePaths *node_removed);
-void update_heap(PossiblePaths *node_removed, Vector *paths, Problem *p, Heap *not_visited);
 
 
 #endif
