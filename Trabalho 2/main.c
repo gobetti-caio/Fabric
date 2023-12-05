5ce46438a4600bd61fbccf1c036b9725a84bdec3
gobetti-caio <124630531+gobetti-caio@users.noreply.github.com>
2023-12-04 23:47:24 -0300

    Update main.c

--- Trabalho 2/main.c
+++ Trabalho 2/main.c
@@ -6,14 +6,14 @@
 #include "dijkstra.h"
 #include "path.h"
 
+
 int main() {
 
     FILE *arq = fopen("entrada.txt", "r");
     Problem *p = read_file(arq);
     Vector *paths = solve_dijkstra(p);
     paths_print(paths);
-
-
+    
     fclose(arq);
     paths_destroy(paths);
     problem_destroy(p);
